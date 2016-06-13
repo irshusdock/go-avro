2f2f3b0cd12a29d088233847f438479ff7308949
irshusdock <ishusdock@gmail.com>
2016-06-13 08:44:18 -0700

    Update datum_writer.go
    Add type assertion

--- datum_writer.go
+++ datum_writer.go
@@ -534,7 +534,7 @@ func (writer *GenericDatumWriter) isWritableAs(v interface{}, s Schema) bool {
 }
 
 func (writer *GenericDatumWriter) writeFixed(v interface{}, enc Encoder, s Schema) error {
-	 enc.WriteRaw(v)
+	 enc.WriteRaw(v.([]byte))
 	 return nil
 }
 
